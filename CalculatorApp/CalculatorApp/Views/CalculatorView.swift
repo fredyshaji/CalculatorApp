dd56e81ee04d0ac9b4c2e96f845c36e5670d16d2
Fredy Shaji <fredy.a.ext.shaji@swedbank.se>
2023-05-14 11:37:41 +0200

    feat: restructure code

--- CalculatorApp/CalculatorApp/Views/CalculatorView.swift
+++ CalculatorApp/CalculatorApp/Views/CalculatorView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+// MARK: - BODY
+
 struct CalculatorView: View {
 
     var buttonTypes: [[ButtonType]] {
@@ -21,29 +23,44 @@ struct CalculatorView: View {
     var body: some View {
         VStack {
             Spacer()
-            Text("0")
-                .padding()
-                .foregroundColor(.white)
-                .frame(maxWidth: .infinity, alignment: .trailing)
-                .font(.system(size: 88, weight: .light))
-                .lineLimit(1)
-                .minimumScaleFactor(0.2)
-            VStack {
-                ForEach(buttonTypes, id: \.self) { row in
-                    HStack {
-                        ForEach(row, id: \.self) { buttonType in
-                            Button(buttonType.description) { }
-                        }
-                    }
-                }
-            }
+            displayText
+            buttonPad
         }
         .background(Color.black)
     }
 }
 
+// MARK: - PREVIEWS
+
 struct CalculatorView_Previews: PreviewProvider {
     static var previews: some View {
         CalculatorView()
+    }
+}
+
+// MARK: - COMPONENTS
+
+extension CalculatorView {
+    
+    private var displayText: some View {
+        Text("0")
+            .padding()
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .font(.system(size: 88, weight: .light))
+            .lineLimit(1)
+            .minimumScaleFactor(0.2)
+    }
+    
+    private var buttonPad: some View {
+        VStack {
+            ForEach(buttonTypes, id: \.self) { row in
+                HStack {
+                    ForEach(row, id: \.self) { buttonType in
+                        Button(buttonType.description) { }
+                    }
+                }
+            }
+        }
     }
 }
