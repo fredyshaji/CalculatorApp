d3b842cc1027e8ce619ce4760e98c88c4b11cdea
Fredy Shaji <fredy.a.ext.shaji@swedbank.se>
2023-05-14 10:39:21 +0200

    feat: Initial commit

---
+++ CalculatorApp/CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Fredy Shaji on 2023-05-14.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
