5c8a54669275187a89081bd660eff797cd003440
Fredy Shaji <fredy.a.ext.shaji@swedbank.se>
2023-05-14 11:46:48 +0200

    feat: Add a hardcoded button style

---
+++ CalculatorApp/CalculatorApp/Styles/CalculatorButtonStyle.swift
@@ -0,0 +1,42 @@
+//
+//  ButtonStyle.swift
+//  Calculator
+//
+//  Created by Fredy Shaji on 2023-05-14.
+//
+
+import SwiftUI
+
+struct CalculatorButtonStyle: ButtonStyle {
+    
+    var size: CGFloat
+    var backgroundColor: Color
+    var foregroundColor: Color
+    
+    func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .font(.system(size: 32, weight: .medium))
+                .frame(width: size, height: size)
+                .background(backgroundColor)
+                .foregroundColor(foregroundColor)
+                .overlay {
+                    if configuration.isPressed {
+                        Color(white: 1.0, opacity: 0.2)
+                    }
+                }
+                .clipShape(Capsule())
+    }
+}
+
+struct CalculatorButtonStyle_Previews: PreviewProvider {
+    static let buttonType: ButtonType = .digit(.six)
+    
+    static var previews: some View {
+        Button(buttonType.description) { }
+            .buttonStyle(CalculatorButtonStyle(
+                size: 80,
+                backgroundColor: buttonType.backgroundColor,
+                foregroundColor: buttonType.foregroundColor)
+            )
+    }
+}
