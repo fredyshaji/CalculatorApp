6c270e4f08ef1adf9ab1ad9cd0b4284d6138a2cb
Fredy Shaji <fredy.a.ext.shaji@swedbank.se>
2023-05-14 10:48:47 +0200

    feat: Rename app and change initial view

---
+++ CalculatorApp/CalculatorApp/CalculatorView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorView.swift
+//  CalculatorApp
+//
+//  Created by Fredy Shaji on 2023-05-14.
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorView()
+    }
+}
